6609e89216c422042e0c47087cd9fde0234da9cb
klisiu <mateusz.klis.krk@gmail.com>
2016-09-27 20:06:11 +0200

    Added mobile menu

--- js/main.js
+++ js/main.js
@@ -1,100 +1,52 @@
-window.onload = function()
-    {
-        document.getElementById("acc-1").addEventListener( 'click' , changeClass );
-        document.getElementById("acc-2").addEventListener( 'click' , changeClass );
-        document.getElementById("acc-3").addEventListener( 'click' , changeClass );
-    }
 
-
-function changeClass1()
-    {
-        if(document.getElementById("aacc-1").className == "accordion-hidden"){
-            document.getElementById("aacc-1").className = "accordion-show";
-        }else{
-            document.getElementById("aacc-1").className = "accordion-hidden";
-        } 
-    }
-
-
-function changeClass2()
-    {
-        if(document.getElementById("aacc-2").className == "accordion-hidden"){
-        	document.getElementById("aacc-2").className = "accordion-show";
-        }else{
-        	document.getElementById("aacc-2").className = "accordion-hidden";
-        } 
-    }
-
-function changeClass3()
-    {
-        if(document.getElementById("aacc-3").className == "accordion-hidden"){
-            document.getElementById("aacc-3").className = "accordion-show";
-        }else{
-            document.getElementById("aacc-3").className = "accordion-hidden";
-        } 
-    }
-
-
-
-/*
-function changeClass()
-    {
-        if(document.getElementById("aacc-2").className == "accordion-hidden"){
-            document.getElementById("aacc-2").className = "accordion-show";
-        }else{
-            document.getElementById("aacc-2").className = "accordion-hidden";
-        } 
-    }
-
-    window.onload = function()
-    {
-        document.getElementById("acc-2").addEventListener( 'click' , changeClass );
-    }
-
-
-
-
-
-
-
-
-
-
-function wyl (){
-	document.getElementById("acc-1").className = "accordion-hidden";
+window.onload = function(){
+        document.getElementById("acc-1").addEventListener( 'click' , changeClass1 );
+        document.getElementById("acc-2").addEventListener( 'click' , changeClass2 );
+        document.getElementById("acc-3").addEventListener( 'click' , changeClass3 );
+        document.getElementById("mobile-ico").addEventListener( 'click' , openMobileMenu );
+        document.getElementById("menu-mobile-list").addEventListener( 'click' , openMobileMenu );
 }
 
-<button onclick="changeClass()">My Button</button>
 
+function changeClass1(){
 
-var acc1 = document.GetElementById("acc-1");
-var acc2 = document.GetElementById("acc-2");
-var acc3 = document.GetElementById("acc-3");
-
-
-console.log(acc1);
-console.log(acc2);
-console.log(acc3);
-
-
-function accbg(acc1){
-		this.classList.toggle("accordion-show");
+    if(document.getElementById("aacc-1").className == "accordion-hidden"){
+        document.getElementById("aacc-1").className = "accordion-show";
+    }else{
+        document.getElementById("aacc-1").className = "accordion-hidden";
+    } 
 }
 
-*/
 
-/* Akordeony działający w HTML
+function changeClass2(){
 
-<script type="text/javascript">
-    function changeClass()
-    {
-        document.getElementById("acc-3").className = "accordion-hidden";
-    }
+    if(document.getElementById("aacc-2").className == "accordion-hidden"){
+    	document.getElementById("aacc-2").className = "accordion-show";
+    }else{
+    	document.getElementById("aacc-2").className = "accordion-hidden";
+    } 
+}
 
-    window.onload = function()
-    {
-        document.getElementById("acc-2").addEventListener( 'click' , changeClass );
-    }
-</script>
+function changeClass3(){
 
-*/
+    if(document.getElementById("aacc-3").className == "accordion-hidden"){
+        document.getElementById("aacc-3").className = "accordion-show";
+    }else{
+        document.getElementById("aacc-3").className = "accordion-hidden";
+        } 
+}
+
+
+function openMobileMenu(){
+    var x = document.getElementById("menu-mobile-list");
+    if (x.className === "menu-list"){
+        x.className += " menu-list2"; 
+        console.log("Menu list is shown");
+    }else
+        {
+        x.className = "menu-list";
+        console.log("Menu list is hidden");
+        }
+}
+
+
