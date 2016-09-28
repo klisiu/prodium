a574475ee609db1f9566b9987893c78e06c62fe8
klisiu <mateusz.klis.krk@gmail.com>
2016-09-28 13:42:14 +0200

    code optimization

--- js/main.js
+++ js/main.js
@@ -1,52 +1,55 @@
 
-window.onload = function(){
-        document.getElementById("acc-1").addEventListener( 'click' , changeClass1 );
-        document.getElementById("acc-2").addEventListener( 'click' , changeClass2 );
-        document.getElementById("acc-3").addEventListener( 'click' , changeClass3 );
-        document.getElementById("mobile-ico").addEventListener( 'click' , openMobileMenu );
-        document.getElementById("menu-mobile-list").addEventListener( 'click' , openMobileMenu );
-}
+function changeClass1() {
 
+    var x = document.getElementById("aacc-1");
 
-function changeClass1(){
-
-    if(document.getElementById("aacc-1").className == "accordion-hidden"){
-        document.getElementById("aacc-1").className = "accordion-show";
-    }else{
-        document.getElementById("aacc-1").className = "accordion-hidden";
+    if (x.className === "accordion-hidden") {
+        x.className = "accordion-show";
+    } else {
+        x.className = "accordion-hidden";
     } 
 }
 
 
-function changeClass2(){
+function changeClass2() {
 
-    if(document.getElementById("aacc-2").className == "accordion-hidden"){
-    	document.getElementById("aacc-2").className = "accordion-show";
-    }else{
-    	document.getElementById("aacc-2").className = "accordion-hidden";
+    var y = document.getElementById("aacc-2");
+
+    if (y.className === "accordion-hidden") {
+        y.className = "accordion-show";
+    } else {
+    	y.className = "accordion-hidden";
     } 
 }
 
-function changeClass3(){
+function changeClass3() {
 
-    if(document.getElementById("aacc-3").className == "accordion-hidden"){
-        document.getElementById("aacc-3").className = "accordion-show";
-    }else{
-        document.getElementById("aacc-3").className = "accordion-hidden";
-        } 
+    var z = document.getElementById("aacc-3"); 
+
+    if (z.className === "accordion-hidden") {
+        z.className = "accordion-show";
+    } else {
+        z.className = "accordion-hidden";
+    } 
 }
 
 
-function openMobileMenu(){
+function openMobileMenu() {
     var x = document.getElementById("menu-mobile-list");
-    if (x.className === "menu-list"){
+    if (x.className === "menu-list") {
         x.className += " menu-list2"; 
         console.log("Menu list is shown");
-    }else
-        {
+    } else {
         x.className = "menu-list";
         console.log("Menu list is hidden");
-        }
+    }
 }
 
 
+window.onload = function () {
+    document.getElementById("acc-1").addEventListener('click', changeClass1);
+    document.getElementById("acc-2").addEventListener('click', changeClass2);
+    document.getElementById("acc-3").addEventListener('click', changeClass3);
+    document.getElementById("mobile-ico").addEventListener('click', openMobileMenu);
+    document.getElementById("menu-mobile-list").addEventListener('click', openMobileMenu);
+}
